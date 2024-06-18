4de362a74869dc67b8f98836ec8665b85b648385
kinuthia-mark <kinuthia.mark@strathmore.edu>
2024-06-18 09:38:42 +0300

    multi colum layout

---
+++ loops.php
@@ -0,0 +1,15 @@
+
+//do while loop
+<h1> do-while loop </h1>
+<?php
+// Initialize the counter variable
+$counter = 1;
+
+do {
+    // Print the current value of counter
+    echo "Counter: $counter\n";
+    print $n;
+    // Increment the counter by 1
+    $counter++;
+} while ($counter <= 5);
+?>
